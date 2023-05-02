2e01f82b4ea5a94dc085bac2d53e9c1d7ad2430e
erickhansx <erickhansx@gmail.com>
2023-05-02 02:24:14 -0600

    Modify render-build

--- render-build.sh
+++ render-build.sh
@@ -3,14 +3,4 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:migrate
-
-#!/usr/bin/env bash
-# exit on error
-# set -o errexit
-
-# bundle install
-# bundle exec rake db:migrate
-# bundle exec rake db:seed
+bundle exec rake db:migrate
