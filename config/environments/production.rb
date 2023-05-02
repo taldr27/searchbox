4d1228de935f4ee1abaee862da5a6e6cb1f0a5da
lana <diegogarciaq98@gmail.com>
2023-05-02 13:39:25 -0500

    fix2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "petbnb_back_production"
 
   config.action_mailer.perform_caching = false
-  routes.default_url_options[:host] = 'https://petbnb.onrender.com/'
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
